42c7b7431f8ea7de2ef0dff0469076e6ab2812bf
Florencia Lorenzati <florencialorenzati@gmail.com>
2026-09-13 23:44:17 -0300

    Feat(Ingreso): Implementación de IngresoActivity y actualización del flujo de navegación #US08
    - Creación de la pantalla IngresoActivity y su diseño visual (activity_ingreso.xml) con botones para Iniciar Sesión, Registrarse e Ingresar como invitado.
    - Modificación de la transición en SplashActivity para redirigir directamente hacia la pantalla de ingreso tras finalizar la carga inicial

---
+++ app/src/main/java/com/ispc/florappmovil/IngresoActivity.java
@@ -0,0 +1,38 @@
+package com.ispc.florappmovil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IngresoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ingreso);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnRegistrarse = findViewById(R.id.btnRegistrarse);
+        Button btnInvitado = findViewById(R.id.btnInvitado);
+
+        // lleva hacia la pantalla de Iniciar Sesión
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(IngresoActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        // lleva hacia la pantalla de Registro
+        btnRegistrarse.setOnClickListener(v -> {
+            Intent intent = new Intent(IngresoActivity.this, RegisterActivity.class);
+            startActivity(intent);
+        });
+
+        // ingreso invitado (va a Galeria)
+        btnInvitado.setOnClickListener(v -> {
+            Intent intent = new Intent(IngresoActivity.this, GaleriaActivity.class);
+            startActivity(intent);
+        });
+    }
+}
